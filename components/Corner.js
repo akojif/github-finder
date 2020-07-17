e28d15ce001d3f402145b9c7bbfe01e922c363cc
Akoji Francis <45937795+akojif@users.noreply.github.com>
2020-07-17 07:45:30 +0100

    Update Corner.js

--- components/Corner.js
+++ components/Corner.js
@@ -48,7 +48,7 @@ const CornerStyles = styled.div`
 const Corner = () => (
   <CornerStyles>
     <a
-      href="https://github.com/akojif/githubfinder"
+      href="https://github.com/akojif/github-finder"
       target="_blank"
       rel="noopener noreferrer"
       className="github-corner"
