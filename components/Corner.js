55c337e251ca37f620d75d9c1cba8bad2e55a8d0
Akoji Francis <jidwithfa007@crmintcol.org>
2020-07-17 07:40:05 +0100

    Update Corner.js

--- components/Corner.js
+++ components/Corner.js
@@ -48,7 +48,7 @@ const CornerStyles = styled.div`
 const Corner = () => (
   <CornerStyles>
     <a
-      href="https://github.com/akojif/octoprofile"
+      href="https://github.com/akojif/githubfinder"
       target="_blank"
       rel="noopener noreferrer"
       className="github-corner"
